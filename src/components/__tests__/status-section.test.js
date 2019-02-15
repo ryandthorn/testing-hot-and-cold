88cb19cc053c271987a79becba690e05274717b0
Ryan Thorn <ryandthorn@gmail.com>
2019-02-15 15:54:46 -0800

    Write tests for StatusSection and TopNav

--- src/components/__tests__/status-section.test.js
+++ src/components/__tests__/status-section.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import StatusSection from '../status-section';
 
 describe('<StatusSection />', () => {
+  const guessArray = [1, 2, 3];
+
   it('should render without crashing', () => {
-    const guessArray = [1, 2, 3];
     shallow(<StatusSection guesses={guessArray} />);
+  });
+
+  it('should be able to access props guesses and auralStatus', () => {
+    const wrapper = mount(<StatusSection guesses={guessArray} auralStatus="test" />);
+    const props = wrapper.props();
+    expect(props.guesses).toEqual(guessArray);
+    expect(props.auralStatus).toEqual('test');
   });
 });
--- src/components/__tests__/top-nav.test.js
+++ src/components/__tests__/top-nav.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import TopNav from '../top-nav';
 
 describe('<TopNav />', () => {
   it('should render without crashing', () => {
     shallow(<TopNav />);
+  });
+
+  it('should fire the onRestartGame callback when New Game is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = mount(<TopNav
+      onRestartGame={callback}
+    />);
+    wrapper.find('.new').simulate('click');
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it('should fire the onGenerateAuralUpdate callback when hidden link is clicked', () => {
+    const callback = jest.fn();
+    const wrapper = mount(<TopNav
+      onGenerateAuralUpdate={callback}
+    />);
+    wrapper.find('.status-link').simulate('click');
+    expect(callback).toHaveBeenCalled();
   });
 });
