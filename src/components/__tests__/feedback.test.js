c6159d3bade90891827ace9f971768bf145d9652
Ryan Thorn <ryandthorn@gmail.com>
2019-02-15 15:23:57 -0800

    Write tests for Feedback

--- src/components/__tests__/feedback.test.js
+++ src/components/__tests__/feedback.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Feedback from '../feedback';
 
 describe('<Feedback />', () => {
   it('should render without crashing', () => {
     shallow(<Feedback />);
+  });
+
+  it('should pass props feedback and guessCount', () => {
+    const message = 'test';
+    const guessCount = 0;
+    const wrapper = mount(<Feedback feedback={message} guessCount={guessCount} />);
+    const props = wrapper.props();
+
+    expect(props.feedback).toEqual(message);
+    expect(props.guessCount).toEqual(guessCount);
+  });
+
+  it('should render a prompt to guess again if on any but the first guess', () => {
+    const message = 'test';
+    const guessCount = 1;
+    const wrapper = mount(<Feedback feedback={message} guessCount={guessCount} />);
+    const wrapperText = wrapper.text()
+
+    expect(wrapperText).toEqual('test Guess again!')
   });
 });
